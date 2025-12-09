edbd0f03f1457059b8c78207569d21d3b9c961a0
edu-jason-kim <edu.jason.kim@gmail.com>
2025-12-09 18:47:51 +0900

    ssg, ssr, isr

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,34 @@
 import SearchForm from "@/components/SearchForm";
 import ProductList from "@/components/ProductList";
-import { useEffect, useState } from "react";
 import axios from "@/lib/axios";
 import Link from "next/link";
 import Head from "next/head";
 
-export default function Home() {
-  const [products, setProducts] = useState([]);
+// SSG
+// export async function getStaticProps() {
+//   const response = await axios.get("/products"); // 9개
+//   const products = response.data.results;
+//   const time = new Date().toISOString();
 
-  useEffect(() => {
-    axios
-      .get("/products")
-      .then((res) => setProducts(res.data.results))
-      .catch(console.error);
-  }, []);
+//   return {
+//     props: { products, time },
+//     // ISR (revalidtae 시간을 주면 끝)
+//     revalidate: 60 * 60 * 24,
+//   };
+// }
 
+// SSR
+export async function getServerSideProps() {
+  const response = await axios.get("/products"); // 9개
+  const products = response.data.results;
+  const time = new Date().toISOString();
+
+  return {
+    props: { products, time },
+  };
+}
+
+export default function Home({ products, time }) {
   return (
     <>
       <Head>
@@ -23,6 +37,7 @@ export default function Home() {
 
       <main>
         <h1>Codeit Mall</h1>
+        <p>{time}</p>
         <Link href="/settings">설정</Link>
         <SearchForm />
         <ProductList products={products} />
--- pages/products/[id]/index.js
+++ pages/products/[id]/index.js
@@ -1,27 +1,40 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import axios from "@/lib/axios";
+import Image from "next/image";
 
-export default function ProductDetail() {
-  const [product, setProduct] = useState(null);
-  const router = useRouter();
-  const { id } = router.query;
+// HTML 정적 생성할 경로를 반환
+export async function getStaticPaths() {
+  const response = await axios.get("/products"); // 9개 응답 -> 9개 만큼만 HTML을 생성
+  const products = response.data.results;
 
-  useEffect(() => {
-    axios
-      .get(`/products/${id}`)
-      .then((res) => setProduct(res.data))
-      .catch(console.error);
-  }, [id]);
+  // 우리가 생성할 페이지 경로들
+  const paths = products.map((product) => {
+    return {
+      params: { id: product.id.toString() },
+    };
+  });
 
-  if (!product) {
-    return <div>로딩 중...</div>;
-  }
+  return {
+    paths,
+    // false: 지정되지 않은 경로는 404 반환
+    // true: 지정되지 않은 경로는 최초 요청시 생성 (로딩 상태표시 필요)
+    // 'blocking': 지정되지 않은 경로는 최초 요청 시 SSR처럼 생성 (로딩 상태 없음)
+    fallback: false,
+  };
+}
 
+// 1~9까지 실행하면서 HTML 생성을 위한 데이터 수집
+export async function getStaticProps({ params }) {
+  const response = await axios.get(`/products/${params.id}`);
+  const product = response.data;
+  return { props: { product } };
+}
+
+// 1~9 product 만큼 HTML 생성
+export default function ProductDetail({ product }) {
   return (
     <main>
       <h1>{product.name}</h1>
-      <img width={300} height={300} src={product.imgUrl} alt={product.name} />
+      <Image width={300} height={300} src={product.imgUrl} alt={product.name} />
     </main>
   );
 }
